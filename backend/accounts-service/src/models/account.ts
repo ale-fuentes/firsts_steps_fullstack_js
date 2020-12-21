047b8ba69fd231bb16acc0a3fc95ad988cd6804c
ale-fuentes <ale.fuentes.misc@gmail.com>
2020-12-20 22:14:21 -0300

    refactor: Add dependencie JOI and new Schema

--- backend/accounts-service/src/models/account.ts
+++ backend/accounts-service/src/models/account.ts
@@ -1,4 +1,4 @@
-export interface IAccount{
+export interface IAccount {
     id: number,
     name: string,
     email: string,
--- backend/accounts-service/src/routes/accounts.ts
+++ backend/accounts-service/src/routes/accounts.ts
@@ -1,9 +1,32 @@
 import { Router, Request, Response } from 'express';
+import Joi from 'joi';
 import accountController from '../controllers/accounts';
+import { accountSchema, loginSchema } from '../models/accountSchema';
+
+function validateSchema(schema: Joi.ObjectSchema<any>, req: Request, res: Response, next: any) {
+    const { error } = schema.validate(req.body);
+    if (error == null) return next();
+
+    const { details } = error;
+    const message = details.map(item => item.message).join(', ');
+
+    console.log(message);
+    res.status(422).end();
+}
+
+function validateAccount(req: Request, res: Response, next: any) {
+    return validateSchema(accountSchema, req, res, next);
+}
+function validateLogin(req: Request, res: Response, next: any) {
+    return validateSchema(loginSchema, req, res, next);
+}
 
 const router = Router();
 router.get('/accounts/', accountController.getAccounts);
 router.get('/accounts/:id', accountController.getAccount);
-router.post('/accounts/', accountController.addAccount);
+router.patch('/accounts/:id', validateAccount, accountController.setAccount);
+router.post('/accounts/', validateAccount, accountController.addAccount);
+router.post('/accounts/login', validateLogin, accountController.loginAccount);
+router.post('/accounts/logout', accountController.logoutAccount);
 
 export default router;
