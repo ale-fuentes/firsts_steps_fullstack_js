ef3a4a5f0505f3225428413b16357b82c27281f7
ale-fuentes <ale.fuentes.misc@gmail.com>
2021-01-08 15:19:06 -0300

    refactor: add path commons

--- backend/accounts-service/src/routes/accounts.ts
+++ backend/accounts-service/src/routes/accounts.ts
@@ -3,6 +3,10 @@ import { Router } from 'express';
 import accountController from '../controllers/accounts';
 import { validateAccountSchema, validateUpdateAccountSchema, validateLoginSchema, validateAuth } from './middlewares';
 
+//example ussing function from __commons__
+import calc from 'ms-commons/calc';
+
+
 const router = Router();
 router.get('/accounts/', validateAuth, accountController.getAccounts);
 router.get('/accounts/:id', validateAuth, accountController.getAccount);
@@ -10,5 +14,13 @@ router.patch('/accounts/:id', validateAuth, validateUpdateAccountSchema, account
 router.post('/accounts/', validateAccountSchema, accountController.addAccount);
 router.post('/accounts/login', validateLoginSchema, accountController.loginAccount);
 router.post('/accounts/logout', accountController.logoutAccount);
+
+//example ussing calc
+router.get('/plusnum/:val1/:val2', (req, res, next) => {
+    const v1 = parseInt(`${req.params.val1}`);
+    const v2 = parseInt(`${req.params.val2}`);
+    const result = calc(v1, v2);
+    res.json({ result })
+})
 
 export default router;
